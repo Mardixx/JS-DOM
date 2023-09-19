ce9d42f05e38d7999677ea1de70c821086363f97
Kimi L <kimi.lefort.pro@gmail.com>
2023-09-19 10:07:03 +0200

    3.2

--- 3.Create/script.js
+++ 3.Create/script.js
@@ -1,21 +1,32 @@
 /*Creating nodes*/
 function randomC() {
-    let red = Math.floor(Math.random()* 256)
-    let green = Math.floor(Math.random()* 256)
-    let blue = Math.floor(Math.random()* 256)
+    let red = Math.floor(Math.random() * 256)
+    let green = Math.floor(Math.random() * 256)
+    let blue = Math.floor(Math.random() * 256)
 
     return color = `rgb(${red}, ${green}, ${blue})`;
 }
+
+function fontcolor(red, green, blue) {
+    let luminance = (red * 299 + green * 587 + blue * 114) / 1000;
+    return luminance < 128 ? 'white' : 'black';
+}
+
+
 
 function people() {
     let nameArray = ["Alexandre Vandewiele", "Antoine Lansman", "Bastien Venturi", "Carole Gérard", "Dorian Vanden Eynde", "Elisabeth Leyder", "Elodie Ali", "Justin Michel", "Justine Frigo", "Justine Leleu", "Kimi Lefort", "Layla", "Lidwine Careme", "Lucas Beauloi", "Marie Tara", "Mathias Barbier", "okly2023", "Pierre Marien", "Robin Piot", "Rosalie Boulard", "Stephane Comblez", "Tim Desmet", "Toms", "Valentin Lefort", "Vens Alexandre", "Virginie Dourson"];
 
     for (child of nameArray) {
-        let randoColor = randomC()
-
         let newSection = document.createElement("section");
 
         let people = document.createElement("p");
+
+        let randomColor = randomC();
+
+        let [red, green, blue] = randomColor.match(/\d+/g).map(Number);
+        
+        let color = fontcolor(red, green, blue);
 
         let name = document.createTextNode(child);
 
@@ -23,10 +34,13 @@ function people() {
 
         newSection.appendChild(people);
 
-        document.querySelector('article').appendChild(newSection).style.backgroundColor = randoColor;
+        people.style.color = color;
+
+        newSection.style.backgroundColor = randomColor;
+
+        document.querySelector('article').appendChild(newSection);
     }
 
 }
 
 people()
-
